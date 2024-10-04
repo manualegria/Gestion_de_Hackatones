eaf963ffb1ecc1e041f0934ce07a69a3c1f61c34
Deymer Mosquera M. <damm.l.27@hotmail.com>
2024-10-04 00:15:15 -0500

    Clases

--- Hackatones.Shared/Entities/TeamParticipant.cs
+++ Hackatones.Shared/Entities/TeamParticipant.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Hackatones.Shered.Entities
 {
-	public team_participant
+	public class TeamParticipant
 	{
         //Foranea equipo
 		[Display(Name = "identificador equipo")]
