e2c0718b102cb8795388b291e1966d523dda1b92
Deymer Mosquera M. <damm.l.27@hotmail.com>
2024-10-06 17:49:54 -0500

    Creacion de Controladores: -participantes -equipos -equipo de parcipantes

---
+++ Hackatones.API/Controllers/HackatonTeamController.cs
@@ -0,0 +1,86 @@
+﻿using Hackatones.API.Data;
+using Hackatones.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hackatones.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/hackatonteam")]
+    public class HackatonTeamsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public HackatonTeamsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.HackatonTeams.ToListAsync());
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var hackatonteam = await _context.HackatonTeams.FirstOrDefaultAsync(x => x.Id == id);
+            if (hackatonteam == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(hackatonteam);
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post(HackatonTeam hackatonteam)
+        {
+            _context.Add(hackatonteam);
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                message = "Se regristro equipo en hackaton.",
+                data = hackatonteam
+            });
+        }
+
+        //Actualizar 
+        [HttpPut]
+        public async Task<ActionResult> Put(Award hackatonteam)
+        {
+            _context.HackatonTeams.Update(hackatonteam);
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                message = "Se Actulizo equipo en hackaton.",
+                data = hackatonteam
+            });
+        }
+
+        // Eliminar 
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            var hackatonteamDelete = await _context.HackatonTeams
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (hackatonteamDelete == 0)
+            {
+                return NotFound(new { message = "Equipo no encontrado en hackaton." });
+            }
+            else
+            {
+                return Ok(new { message = "Equipo eliminado de la hackaton." });
+            }
+        }
+    }
+}
---
+++ Hackatones.API/Controllers/TeamsController.cs
@@ -0,0 +1,86 @@
+﻿using Hackatones.API.Data;
+using Hackatones.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hackatones.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/team")]
+    public class TeamsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TeamsController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Teams.ToListAsync());
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var team = await _context.Teams.FirstOrDefaultAsync(x => x.Id == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(team);
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Team team)
+        {
+            _context.Add(team);
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                message = "Equipo creado.",
+                data = team
+            });
+        }
+
+        //Actualizar 
+        [HttpPut]
+        public async Task<ActionResult> Put(Team team)
+        {
+            _context.Teams.Update(team);
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                message = "Se actualizo equipo.",
+                data = team
+            });
+        }
+
+        // Eliminar 
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var teamDelete = await _context.Teams
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (teamDelete == 0)
+            {
+                return NotFound(new { message = "Equipo no encontrado." });
+            }
+            else
+            {
+                return Ok(new { message = "Equipo  eliminado." });
+            }
+        }
+    }
+}
