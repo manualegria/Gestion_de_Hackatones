98f6d84d5957602a8f9330c177244ba1840c9be8
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-10-06 15:20:05 -0500

    Revert "Delete Hackatones.Shared/Entities/participant.cs"
    This reverts commit 20d6074842944a43ba1c043d68d825235a7b6dcc.

---
+++ Hackatones.Shared/Entities/participant.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Hackatones.Shered.Entities
+{
+    public class Participant
+	{
+		
+		public int Id { get; set; }
+
+		[Display(Name = "Nombre del participante")]
+		[MaxLength(50, ErrorMessage = "El {0}, no puede tener mas de 50 caractere.")]
+		[Required(ErrorMessage = "El {0} es obligatorio")]
+		public string Name { get; set; }
+
+		[Display(Name = "Apellido participante")]
+		[MaxLength(50, ErrorMessage = "El {0}, no puede tener mas de 50 caractere.")]
+		[Required(ErrorMessage = "El {0} es obligatorio")]
+		public string LastName { get; set; }
+
+        [Display(Name = "Correo participante")]
+        [MaxLength(50, ErrorMessage = "El {0}, no puede tener mas de 50 caractere.")]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        public string Email { get; set; }
+
+        [Display(Name = "Numero celular participante")]
+        [MaxLength(50, ErrorMessage = "El {0}, no puede tener mas de 50 caractere.")]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        public string phone { get; set; }
+
+        [Display(Name = "Experiencia en desarrollo")]
+        [MaxLength(50, ErrorMessage = "El {0}, no puede tener mas de 50 caractere.")]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        public string developmentExperience { get; set; }
+
+        [Display(Name = "Experiencia en diseño")]
+        [MaxLength(50, ErrorMessage = "El {0}, no puede tener mas de 50 caractere.")]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        public string experienceDesign { get; set; }
+
+        [Display(Name = "Experiencia gestion")]
+        [MaxLength(50, ErrorMessage = "El {0}, no puede tener mas de 50 caractere.")]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        public string managementExperience { get; set; }
+
+
+        [JsonIgnore]
+        public ICollection<TeamParticipant> TeamParticipants { get; set; }
+    }
+}
